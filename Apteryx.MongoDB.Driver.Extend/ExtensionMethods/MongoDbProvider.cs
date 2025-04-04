d83357381f39b665188680342bb8f30787213ac1
王苑 <wyspaces@outlook.com>
2025-04-04 22:31:24 +0800

    更新 mongo sdk

--- Apteryx.MongoDB.Driver.Extend/ExtensionMethods/MongoDbProvider.cs
+++ Apteryx.MongoDB.Driver.Extend/ExtensionMethods/MongoDbProvider.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 
 namespace Apteryx.MongoDB.Driver.Extend
 {
-    public abstract partial class MongoDbProvider
+    public abstract class MongoDbProvider
     {
         //定义数据库
         public IMongoDatabase Database { get; set; }
@@ -34,13 +34,13 @@ namespace Apteryx.MongoDB.Driver.Extend
 
         private void InitializeDbSets()
         {
-            var properties = GetType().GetProperties()
-                .Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
-
-            foreach (var property in properties)
+            var properties = GetType().GetProperties();
+            var dbSetProperties = properties.Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+            foreach (var property in dbSetProperties)
             {
-                var entityType = property.PropertyType.GetGenericArguments()[0];
                 var collectionName = property.Name;
+                var arguments = property.PropertyType.GetGenericArguments();
+                var entityType = arguments[0];
                 var dbSetType = typeof(DbSet<>).MakeGenericType(entityType);
                 var dbSetInstance = Activator.CreateInstance(dbSetType, Database, collectionName);
                 property.SetValue(this, dbSetInstance);
@@ -50,17 +50,16 @@ namespace Apteryx.MongoDB.Driver.Extend
         private void InitializeCollections()
         {
             var properties = this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var property in properties)
+            var collectionProperties = properties.Where(p => p.PropertyType.IsGenericType && p.PropertyType.GetGenericTypeDefinition() == typeof(IMongoCollection<>));
+            foreach (var property in collectionProperties)
             {
-                if (property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(IMongoCollection<>))
-                {
-                    var collectionName = property.Name;
-                    var collectionType = property.PropertyType.GetGenericArguments()[0];
-                    var method = typeof(IMongoDatabase).GetMethod("GetCollection", new[] { typeof(string), typeof(MongoCollectionSettings) });
-                    var genericMethod = method.MakeGenericMethod(collectionType);
-                    var collection = genericMethod.Invoke(Database, new object[] { collectionName, null });
-                    property.SetValue(this, collection);
-                }
+                var collectionName = property.Name;
+                var arguments = property.PropertyType.GetGenericArguments();
+                var entityType = arguments[0];
+                var method = typeof(IMongoDatabase).GetMethod("GetCollection", new[] { typeof(string), typeof(MongoCollectionSettings) });
+                var genericMethod = method.MakeGenericMethod(entityType);
+                var collection = genericMethod.Invoke(Database, new object[] { collectionName, null });
+                property.SetValue(this, collection);
             }
         }
     }
